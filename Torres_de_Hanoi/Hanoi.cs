c45054e21fcbdce5ca13c978ffd9b12b15d10853
Laura <laurapamiesvillagordo@gmail.com>
2022-03-09 18:34:26 +0100

    implementado metodo de mover disco

--- Torres_de_Hanoi/Hanoi.cs
+++ Torres_de_Hanoi/Hanoi.cs
@@ -6,18 +6,56 @@ using System.Threading.Tasks;
 
 namespace Torres_de_Hanoi
 {
-    class Hanoi
+    static class Hanoi
     {
+
+        private static int num_discos; //es privado porque solo lo consulta la solución
+        private static Pila INI, FIN, AUX;
+        private static int num_movimientos;
+        
         /*TODO: Implementar métodos*/
-        public void mover_disco(Pila a, Pila b)
+        private static void mover_disco(Pila a, Pila b)
         {
+          if (solucion()==false) { //mientras no haya solución
+            //1º caso: a --> b
+            if(a.isEmpty() == false) //si a no está vacia
+            {
+                if(b.isEmpty() == true) //si b  está vacia
+                {
+                    b.push(a.pop()); // metemos en b el ultimo disco de a
+                    num_movimientos++;
+                    Console.WriteLine(a.Nombre + " - " + b.Top.Valor + " -> " + b.Nombre);
 
+                }
+                else if(a.Top.Valor < b.Top.Valor) //se comparan los valores de los discos superiores
+                {
+                    b.push(a.pop()); // metemos en b el ultimo disco de a
+                    num_movimientos++;
+                    Console.WriteLine(a.Nombre + " - " + b.Top.Valor + " -> " + b.Nombre);
+
+                }
+                else //si a.top > b.top
+                {   
+                    a.push(b.pop());
+                    num_movimientos++;
+                    Console.WriteLine(b.Nombre + " - " + a.Top.Valor + " -> " + a.Nombre);
+
+                }
+            }
+            //2º caso: b-->a
+            else //si a está vacia
+            {
+                if (b.isEmpty() == false) //si b no está vacia 
+                {
+                    a.push(b.pop());
+                    num_movimientos++;
+                    Console.WriteLine(b.Nombre + " - " + a.Top.Valor + " -> " + a.Nombre);
+
+                } 
+            }
+          }
         }
 
-        public int iterativo(int n, Pila ini, Pila fin, Pila aux)
-        {
-            return 0;
-        }
-
+       
     }
 }
--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,10 +10,21 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Introduce el numero de discos");
+            int numerodiscos = Convert.ToInt32(Console.ReadLine()); //Leer la tecla introducida por el usuario y pasarla de String a Int
 
+            Pila INI = new Pila("INI",numerodiscos); //pila llena con el número de discos que asignamos
+            Pila AUX = new Pila("AUX"); //pila vacia
+            Pila FIN = new Pila("FIN"); //pila vacia
+
+            
+
+            Console.WriteLine("El número de movimientos es: " + numeromovimientos);
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.ReadKey(); //Espera a que se pulse una tecla (para que no se cierre la consola)
+
         }
+
     }
 }
